069174021754d1156fe4df8b31c5e7cf6f819e76
Matt <matthew-casey@lambdastudents.com>
2021-07-15 21:43:48 -0500

    did more testing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,18 +5,38 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm/>)
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm/>)
+    const item = screen.queryByText(/contact form/i)
+    expect(item).toBeInTheDocument()
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
+    render(<ContactForm/>);
+    const firstNameInput = screen.getByLabelText('First Name*')
+        userEvent.type(firstNameInput, 'We');
+        const firstNameError = screen.getByText(/firstName must have at least 5 characters./)
+        expect(firstNameError).toBeInTheDocument()
+    
+
     
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
+    render(<ContactForm/>)
+    const firstNameInput = screen.getByLabelText('First Name*')
+    const MessageInput = screen.getByLabelText('Message')
+    const EmailInput = screen.getByLabelText('Email*')
+    userEvent.type(firstNameInput, '1')
+    userEvent.type(MessageInput, '')
+    userEvent.type(EmailInput, '')
+    expect(screen.getByText(/firstName must have at least 5 characters./i)).toBeInTheDocument()
+    expect(screen.getByText(/email must be a valid email address./i)).toBeInTheDocument()
+    expect(screen.getByText(/message is a required field./i)).toBeInTheDocument()
+
     
 });
 

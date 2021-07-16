ac85bd80554209786d16b40d9a242505b5e4581e
Matt <matthew-casey@lambdastudents.com>
2021-07-15 22:53:07 -0500

    finished MVP

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,57 +5,124 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    render(<ContactForm/>)
+    render(<ContactForm/>);
 });
 
 test('renders the contact form header', ()=> {
-    render(<ContactForm/>)
-    const item = screen.queryByText(/contact form/i)
-    expect(item).toBeInTheDocument()
+    render(<ContactForm/>);
+    const header = screen.queryByText(/contact form/i);
+    expect(header).toBeInTheDocument();
+    expect(header).toBeTruthy();
+    expect(header).toHaveTextContent(/contact form/i);
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
     render(<ContactForm/>);
-    const firstNameInput = screen.getByLabelText('First Name*')
+    const firstNameInput = screen.getByLabelText(/First Name*/i)
         userEvent.type(firstNameInput, 'We');
-        const firstNameError = screen.getByText(/firstName must have at least 5 characters./)
-        expect(firstNameError).toBeInTheDocument()
-    
-
-    
+        const firstNameError = await screen.findAllByTestId('error')
+        expect(firstNameError).toHaveLength(1);
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    render(<ContactForm/>)
-    const firstNameInput = screen.getByLabelText('First Name*')
-    const MessageInput = screen.getByLabelText('Message')
-    const EmailInput = screen.getByLabelText('Email*')
-    userEvent.type(firstNameInput, '1')
-    userEvent.type(MessageInput, '')
-    userEvent.type(EmailInput, '')
-    expect(screen.getByText(/firstName must have at least 5 characters./i)).toBeInTheDocument()
-    expect(screen.getByText(/email must be a valid email address./i)).toBeInTheDocument()
-    expect(screen.getByText(/message is a required field./i)).toBeInTheDocument()
-
-    
+  render(<ContactForm />);
+  const button = screen.getByRole('button')
+  userEvent.click(button);
+  const errormsg = await screen.findAllByTestId('error');
+  expect(errormsg).toHaveLength(3);
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
+    render(<ContactForm />);
+    const button = screen.getByRole('button');
+    const firstNameInput = screen.getByLabelText(/First Name*/i)
+    const LastNameInput = screen.getByLabelText(/Last Name*/i)
+    userEvent.type(firstNameInput,'Matthew');
+    userEvent.type(LastNameInput,'Casey');
+    userEvent.click(button);
+    const errormsg = await screen.getAllByTestId('error');
+    expect(errormsg).toHaveLength(1);
     
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
+    render(<ContactForm/>);
+    const emailInput = screen.getByLabelText(/Email*/i);
+    userEvent.type(emailInput, 'asdfasdfa');
+    const errorMsg = await screen.findByText(/email must be a valid email address/i);
+    expect(errorMsg).toBeInTheDocument();
     
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
+    render(<ContactForm/>);
+    const FirstNameInput = screen.getByLabelText(/first name*/i);
+        userEvent.type(FirstNameInput, 'Matthew');
+
+    const EmailInput = screen.getByLabelText(/email*/i);
+        userEvent.type(EmailInput, 'mattcasey879@gmail.com');
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const errorMsg = await screen.findByText(/lastName is a required field/i)
+    expect(errorMsg).toBeInTheDocument();
+
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
+    render(<ContactForm/>)
+    const firstNameInput = screen.getByLabelText(/first name*/i);
+        userEvent.type(firstNameInput, 'Matthew');
+        
+    const lasttNameInput = screen.getByLabelText(/last name*/i);
+        userEvent.type(lasttNameInput, 'Casey');
     
+    const emailInput = screen.getByLabelText(/email*/i)
+        userEvent.type(emailInput, 'mattcasey879@gmail.com');
+    
+    const button = screen.getByRole('button');
+        userEvent.click(button);
+    
+     waitFor(()=> {
+        const firstName = screen.queryByText('Matthew');
+        const lastName = screen.queryByText('Casey');
+        const email = screen.queryByText('mattcasey879@gmail.com');
+        const message = screen.getAllByTestId('messageDisplay');
+        
+        expect(firstName).toBeInTheDocument();
+        expect(lastName).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+        expect(message).not.toBeInTheDocument();
+    })
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
+    render(<ContactForm/>)
+    const firstNameInput = screen.getByLabelText(/first name*/i);
+        userEvent.type(firstNameInput, 'Matthew');
+        
+    const lasttNameInput = screen.getByLabelText(/last name*/i);
+        userEvent.type(lasttNameInput, 'Casey');
     
+    const emailInput = screen.getByLabelText(/email*/i)
+        userEvent.type(emailInput, 'mattcasey879@gmail.com');
+
+    const messageInput = screen.getByLabelText(/message/i)
+        userEvent.type(messageInput, 'This is my message. YAY')
+    
+    const button = screen.getByRole('button');
+        userEvent.click(button);
+    
+     waitFor(()=> {
+        const firstName = screen.queryByText('Matthew');
+        const lastName = screen.queryByText('Casey');
+        const email = screen.queryByText('mattcasey879@gmail.com');
+        const message = screen.getAllByTestId('messageDisplay');
+        
+        expect(firstName).toBeInTheDocument();
+        expect(lastName).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+        expect(message).toBeInTheDocument();
+    })
 });
